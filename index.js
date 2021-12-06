2ceb8c6dd89c34e1fcda834a127a849a2cfa53ff
arayra <amandavianiva@outlook.com>
2021-12-06 20:16:09 +0700

    argument parsing with yargs

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-const command = process.argv[2];
+const yargs = require("yargs");
 
-if (command === "add") {
-  console.log("Menambahkan file...");
-} else if (command === "remove") {
-  console.log("Menghapus file...");
-} else {
-  console.log("Perintah tidak dikenali, silahkan mencoba kembali!");
-}
+// Create add and remove command
+yargs.command([
+  {
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true, // Agar menjadi required
+        type: "string",
+      },
+      body: {
+        describe: "Note body",
+        demandOption: true, // Default is false
+        type: "string",
+      },
+    },
+    handler: (argv) => {
+      console.log("Title: " + argv.title + "\nBody: " + argv.body);
+    },
+  },
+  {
+    command: "remove",
+    describe: "Remove a note",
+    handler: () => {
+      console.log("Remove a note...");
+    },
+  },
+  {
+    command: "list",
+    describe: "List the notes",
+    handler: () => {
+      console.log("Listing the notes...");
+    },
+  },
+  {
+    command: "read",
+    describe: "Read the note",
+    handler: () => {
+      console.log("Try to reading the note...");
+    },
+  },
+]);
+
+console.log(yargs.argv);
